ed8fe242d17bd8ea738a7b47b6f58710fd15b182
c00lhawk607 <jordandixon2022@lebanon.k12.mo.us>
2020-05-11 14:10:40 +0000

    A bunch.

--- clearing/setup.py
+++ clearing/setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
 name='clearing',
-version='0.1',
+version='0.0.1',
 description='Testing installation of Package',
+long_description=long_description
+long_description_content_type="text/markdown"
 url='https://github.com/c00lhawk607/Clearing',
 author='Jordan Dixon',
 author_email='jordandixon2004@outlook.com',
 license='GPL v3',
 packages=['clearing'],
-zip_safe=False)
+classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: GPL :: 3",
+  "Operating System :: OS independent",
+],
+zip_safe=False,
+python_requires=">=3.0",
+)
